6db604c3f5d6aae1b17e28be2228462cdca62f49
Sergey <kuznetsov.s.i@bk.ru>
2022-09-14 19:06:18 +0300

    4. Скачивание файла с ограничением. [#144271 #343225]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -9,25 +9,31 @@ public class Wget implements Runnable {
 
     static private final int ONE_SECOND_IN_MILLISECONDS = 1000;
 
-    private final String readPath;
+    private final String url;
 
     private final int speed;
 
-    public Wget(String path, int speed) {
-        this.readPath = path;
+    private final String file;
+
+    public Wget(String url, int speed, String file) {
+        this.url = url;
         this.speed = speed;
+        this.file = file;
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length < 2) {
+        if (args.length < 3) {
             throw new IllegalArgumentException(String.format(
-                    "Specify URL and download delay format %s %s",
-                    "HTTP://SITE.COM/.../FILE", "NUMBER_IN_MILLISECONDS"
+                    "Specify URL, download delay, file for downloaded data format %s %s %s",
+                    "HTTP://SITE.COM/.../FILE.EXTENSION",
+                    "NUMBER_IN_MILLISECONDS",
+                    "FILE.EXTENSION"
             ));
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String file = args[2];
+        Thread wget = new Thread(new Wget(url, speed, file));
         wget.start();
         wget.join();
     }
@@ -35,23 +41,25 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(
-                new URL(readPath).openStream()
+                new URL(url).openStream()
         );
-             FileOutputStream out = new FileOutputStream(url)) {
+             FileOutputStream out = new FileOutputStream(file)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
             int downloadData = 0;
             long startTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                downloadData += bytesRead;
+                downloadData += dataBuffer.length;
+                out.write(dataBuffer, 0, bytesRead);
                 if (downloadData >= speed) {
-                    long endTime = System.currentTimeMillis();
-                    if ((endTime - startTime) < ONE_SECOND_IN_MILLISECONDS) {
-                        Thread.sleep(ONE_SECOND_IN_MILLISECONDS
-                                - (endTime - startTime));
+                    long nowTime = System.currentTimeMillis();
+                    System.out.println(nowTime - startTime);
+                    if ((nowTime - startTime) < ONE_SECOND_IN_MILLISECONDS) {
+                        Thread.sleep(ONE_SECOND_IN_MILLISECONDS - (nowTime - startTime));
+                        downloadData = 0;
+                        startTime = System.currentTimeMillis();
                     }
                 }
-                out.write(dataBuffer, 0, bytesRead);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
