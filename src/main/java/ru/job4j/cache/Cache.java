3bc9b1d85cfbf921093979ab1f8492e3a9c7ccb3
Sergey <kuznetsov.s.i@bk.ru>
2022-09-15 19:52:22 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,27 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Cache {
+
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.getId(), model) == null;
+    }
+
+    public boolean update(Base model) {
+        Base stored = memory.get(model.getId());
+        if (stored.getVersion() != model.getVersion()) {
+            throw new OptimisticException(
+                    "Version are not equal"
+            );
+        }
+        return false;
+    }
+
+    public void delete(Base model) {
+
+    }
+}
