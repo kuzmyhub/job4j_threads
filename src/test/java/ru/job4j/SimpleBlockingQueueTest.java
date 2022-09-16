ae7cb565276dd64ff998efb13986147dfb3fcf7e
Kuznetsov. S. I <kuznetsov.s.i@bk.ru>
2022-09-16 08:14:36 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/SimpleBlockingQueueTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -74,5 +76,37 @@ class SimpleBlockingQueueTest {
             Thread.currentThread().interrupt();
         }
         assertThat(queue.poll()).isEqualTo(3);
+    }
+
+    @Test
+    public void whenPollThen12345() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer
+                = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue queue = new SimpleBlockingQueue(5);
+        List<Integer> in = List.of(1, 2, 3, 4, 5);
+        Thread producer = new Thread(() -> {
+            try {
+                for (Integer i : in) {
+                    queue.offer(i);
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+        producer.start();
+        Thread consumer = new Thread(() -> {
+            while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                try {
+                    buffer.add((Integer) queue.poll());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        consumer.start();
+        consumer.interrupt();
+        producer.join();
+        consumer.join();
+        assertThat(buffer).isEqualTo(List.of(1, 2, 3, 4, 5));
     }
 }
